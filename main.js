f2ff26ee9322a57d19dbba750d413c2edc985567
mc0749050 <131736467+mc0749050@users.noreply.github.com>
2024-06-13 18:55:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+function goFun() {
+ let userInput = document.getElementById("user").value;
+userInput = userInput.toLowerCase();
+userInput = userInput.replace(
+ / /g,
+ ""
+);
+
+if (userInput == "codewithmukesh") {
+ window.open("https://codewithmukesh.online/");
+}
+else if (userInput == "travelromio") {
+window.open("http://travelromio.online/");
+} 
+
+else if (userInput == "gallery") {
+window.open("https://mc0749050.github.io/Photo-gallery-/");
+} 
+
+else if (userInput == "intrestcalculator") {
+ window.open("https://mc0749050.github.io/Intrest-calculator-/");
+}
+
+else if (userInput == "love") {
+window.open("https://mc0749050.github.io/Love---website-/");
+} 
+
+else if (userInput == "music") {
+ window.open("https://mc0749050.github.io/Music---website-/");
+}
+
+else if (userInput == "food") {
+window.open("https://mc0749050.github.io/Food-ecomerce-website-/");
+} 
+
+else if (userInput == "cricket") {
+ window.open("https://mc0749050.github.io/Cricket-Game/");
+}
+else if (userInput == "calculator") {
+ window.open("https://mc0749050.github.io/Calculator/");
+}
+
+else{
+ alert("website not found");
+}
+ 
+}
